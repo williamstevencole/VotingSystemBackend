fbe3c349f92b2fc828b16a7fd2d12d4c85bb283a
coleexz <williamstevencole@gmail.com>
2025-06-28 18:07:39 -0600

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -9,6 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
